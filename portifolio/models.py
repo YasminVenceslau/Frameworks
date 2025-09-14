fb1393dab64fad3aaa06ac535c9f67cb061ed608
Yasmin Venceslau <yasmin.venceslau@hotmail.com>
2025-09-14 17:50:32 -0300

    Cria modelos Post e UserProfile para o portfólio

--- portifolio/models.py
+++ portifolio/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+# Modelo de posts do portfólio
+class Post(models.Model):
+    titulo = models.CharField(max_length=100)
+    conteudo = models.TextField()
+
+    def __str__(self):
+        return self.titulo
+
+# Modelo de usuários do portfólio
+class UserProfile(models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.nome
