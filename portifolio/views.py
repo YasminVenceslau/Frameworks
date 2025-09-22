01ee1ceb46cfe6b369f6ef221700bc48e875a4ea
Yasmin Venceslau <yasmin.venceslau@hotmail.com>
2025-09-22 17:51:31 -0300

    i

--- portifolio/views.py
+++ portifolio/views.py
@@ -1,17 +1,16 @@
 # portifolio/views.py
-
-from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import DetailView
-from .models import Post  # Certifique-se de que o modelo Post existe
+from .models import Post
 
-# Função para a página inicial
+# Página inicial mostrando apenas posts publicados
 def home(request):
-    posts = Post.objects.all().order_by('-criado_em')  # pega todos os posts mais recentes primeiro
+    posts = Post.objects.filter(status='published').order_by('-criado_em')
     return render(request, 'index.html', {'posts': posts})
 
-# Classe para visualizar detalhes de um post
-# Só habilite se você tiver o modelo Post e um template post_detail.html
 class PostDetails(DetailView):
-    model = Post  # modelo que será exibido
-    template_name = 'post_detail.html'  # template que será usado
+    model = Post
+    template_name = 'post_detail.html'
+    context_object_name = 'post'
+    slug_field = 'slug'         # campo do modelo usado para lookup
+    slug_url_kwarg = 'slug'     # nome do parâmetro na URL
